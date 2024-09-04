154dd1ffe31569d3c1c05e260d4490ef9dd513c1
COIRO AGUSTIN FRANCO <95722600+AFCoiro@users.noreply.github.com>
2024-09-04 17:45:35 -0300

    pequeño cambio en oficina sin git

--- scripts/us.js
+++ scripts/us.js
@@ -2,17 +2,18 @@ const arrows = document.querySelectorAll('.arrowRight');
 const desplegables = document.querySelectorAll('.desplegable');
 const helpItem = document.querySelectorAll('.helpItem');
 
-arrows.forEach((arrow, i) => {
-    arrow.addEventListener('click', function() {
-        // Cerrar todos los desplegables
+helpItem.forEach((help, i) => {
+    help.addEventListener('click', function() {
         desplegables.forEach((desplegable, j) => {
             if (i !== j) {
                 desplegable.style.display = 'none';
+                arrows[j].classList.remove('rotated');
             }
         });
-
-        // Abrir el desplegable correspondiente al arrow clickeado
         const desplegable = desplegables[i];
         desplegable.style.display = desplegable.style.display === 'none' || desplegable.style.display === '' ? 'block' : 'none' ;
+        arrows[i].classList.toggle('rotated');
     });
 });
+
+
