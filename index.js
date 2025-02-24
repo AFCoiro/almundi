44390ae4644b6d7de7c8a86b6cf5ea7a6a3997ac
COIRO AGUSTIN FRANCO <95722600+AFCoiro@users.noreply.github.com>
2025-02-24 18:05:34 +0000

    arreglo index.html y nuevo navbar

---
+++ index.js
@@ -0,0 +1,17 @@
+const navbarToggle = navbar.querySelector("#navbar-toggle");
+const navbarMenu = document.querySelector("#navbar-menu");
+const navbarLinksContainer = navbarMenu.querySelector(".navbar-links");
+let isNavbarExpanded = navbarToggle.getAttribute("aria-expanded") === "true";
+
+const toggleNavbarVisibility = () => {
+  isNavbarExpanded = !isNavbarExpanded;
+  navbarToggle.setAttribute("aria-expanded", isNavbarExpanded);
+
+  // Agregar o quitar la clase que bloquea el scroll
+  document.body.classList.toggle("no-scroll", isNavbarExpanded);
+};
+
+navbarToggle.addEventListener("click", toggleNavbarVisibility);
+
+navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
+navbarMenu.addEventListener("click", toggleNavbarVisibility);
