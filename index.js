3dd720c194452cec15fcfe856c67cfb229bb1108
COIRO AGUSTIN FRANCO <95722600+AFCoiro@users.noreply.github.com>
2025-02-25 20:48:05 +0000

    cambios en search.htmly en json

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
+const navbar = document.querySelector("#navbar");
 const navbarToggle = navbar.querySelector("#navbar-toggle");
 const navbarMenu = document.querySelector("#navbar-menu");
 const navbarLinksContainer = navbarMenu.querySelector(".navbar-links");
+
 let isNavbarExpanded = navbarToggle.getAttribute("aria-expanded") === "true";
 
 const toggleNavbarVisibility = () => {
   isNavbarExpanded = !isNavbarExpanded;
   navbarToggle.setAttribute("aria-expanded", isNavbarExpanded);
 
-  // Agregar o quitar la clase que bloquea el scroll
-  document.body.classList.toggle("no-scroll", isNavbarExpanded);
+  // Solo bloquear scroll si el botón hamburguesa es visible
+  if (window.innerWidth <= 768) {
+    document.body.classList.toggle("no-scroll", isNavbarExpanded);
+  }
 };
 
+// Evento para el botón hamburguesa
 navbarToggle.addEventListener("click", toggleNavbarVisibility);
 
+// Evitar que los clics dentro del menú lo cierren
 navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
-navbarMenu.addEventListener("click", toggleNavbarVisibility);
+
+// Solo cerrar el menú cuando se haga click fuera de los enlaces
+navbarMenu.addEventListener("click", () => {
+  if (window.innerWidth <= 768) {
+    toggleNavbarVisibility();
+  }
+});
